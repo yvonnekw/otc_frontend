2f6eee1e4e07e9daba3aa0571264eb18d0fd40e4
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-04-15 09:18:36 +0100

    fix fill not found ci error

---
+++ src/components/notFound/NotFound.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const NotFound = () => {
+    
+    return (
+    <>
+    <h1>Not found</h1>
+        <Link to ="/">Go to Homepage</Link>
+    </>
+    )
+}
+
+export default NotFound
