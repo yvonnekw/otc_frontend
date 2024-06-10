8ad9052cc64b6ed29c0e227dd535f7d008a88dfe
Yvonne Kwarteng <yvonnekcnwl@yahoo.co.uk>
2024-06-10 18:10:41 +0100

    for ci environment

--- src/services/ApiUtils.ts
+++ src/services/ApiUtils.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import jwt_decode from "jwt-decode";
 
-export const REST_API_BASE_URL: string = "http://localhost:8000";
+export const REST_API_BASE_URL: string = process.env.REST_API_BASE_URL || '' + process.env.REST_API_PORT;
 export const basicHeader = {
   "Content-Type": "application/json",
 };
